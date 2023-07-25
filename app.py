7a5c2492b47699acf119f51d7ffed8ece5fa2c6f
Ejiro Onose <ejiro10000@gmail.com>
2023-07-25 23:07:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ st.subheader('Upload the Picture of your Dream House and get a predicted price.'
 
 
 
-submit_button = st.form_submit_button(label="Upload")
+#submit_button = st.form_submit_button(label="Upload")
