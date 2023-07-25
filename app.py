95008bbb0386593f75fe294946edf7c028418413
Ejiro Onose <ejiro10000@gmail.com>
2023-07-25 23:06:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-import 
+import streamlit as st
+
+st.title('Dream House Predictor')
+
+st.subheader('Upload the Picture of your Dream House and get a predicted price.')
+
+
+
+
+submit_button = st.form_submit_button(label="Upload")
