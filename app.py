9c47618138c7db231bbbd3c0d70cc3d092e0a88e
Ejiro Onose <ejiro10000@gmail.com>
2023-07-25 23:23:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ st.title('Dream House Predictor')
 
 st.subheader('Upload the Picture of your Dream House and get a predicted price.')
 
-
-
+uploaded_file = st.file_uploader("Choose an image")
 
 st.button(label="Upload your House Image ")
